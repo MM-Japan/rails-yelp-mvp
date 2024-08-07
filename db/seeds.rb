6983b322635f10eec233dde70515a5d59796c855
Max McCain <itsmaximuk@gmail.com>
2024-08-07 16:20:31 +0900

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "destroying data..."
+Restaurant.destroy_all
+Review.destroy_all
+
+puts "seeding..."
+10.times do
+  @restaurant = Restaurant.new({
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    category: Restaurant::CATEGORIES.sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  })
+
+  3.times do
+    Review.create!({
+      content: Faker::Lorem.paragraph,
+      rating: rand(0..5),
+      restaurant: @restaurant
+    })
+  end
+end
+
+
+puts "...done!"
